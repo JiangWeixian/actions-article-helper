1cc25c3038072f7fe141fcb2c998a3147f062fa4
JiangWeixian <jiangweixian1994@gmail.com>
2023-02-11 20:47:45 +0800

    :tada:  tada

---
+++ test/index.test.ts
@@ -0,0 +1,14 @@
+import wait from '../src/wait'
+import { expect, test } from 'vitest'
+
+test('throws invalid number', async () => {
+  await expect(wait('foo')).rejects.toThrow('milliseconds not a number')
+})
+
+test('wait 500 ms', async () => {
+  const start = new Date()
+  await wait(500)
+  const end = new Date()
+  const delta = Math.abs(end.valueOf() - start.valueOf())
+  expect(delta).toBeGreaterThanOrEqual(500)
+})
