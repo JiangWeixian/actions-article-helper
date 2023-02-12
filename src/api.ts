d688a626b0410e0a32c366c57857c53adf8ef570
JiangWeixian <jiangweixian1994@gmail.com>
2023-02-12 11:44:12 +0800

    :bug:  node fetch polyfill

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,7 @@
 import { ChatGPTAPI } from 'chatgpt'
+import { fetch } from 'cross-fetch'
+
+globalThis.fetch = fetch
 
 export const createChatGPTAPI = (apiKey: string) => {
   return new ChatGPTAPI({
