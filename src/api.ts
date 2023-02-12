1963b5e04941517e8deef746bae0010c7a990eb8
JiangWeixian <jiangweixian1994@gmail.com>
2023-02-12 12:05:29 +0800

    :bug:  patch global fetch

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,4 @@
 import { ChatGPTAPI } from 'chatgpt'
-import { fetch } from 'cross-fetch'
-
-globalThis.fetch = fetch
 
 export const createChatGPTAPI = (apiKey: string) => {
   return new ChatGPTAPI({
