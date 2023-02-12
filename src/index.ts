dc46e4ff2a5e0ef1b63e7fa082b0bd9621a0e0e3
JiangWeixian <jiangweixian1994@gmail.com>
2023-02-12 11:01:49 +0800

    :construction:  prompts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import Debug from 'debug'
-// import { template } from 'lodash-es'
+import { template } from 'lodash-es'
 
 const debug = Debug('neo:article-helper')
 
 const COMMENT_AUTHOR = new Set(['github-actions[bot]'])
 const prefix = '<!--article-helper-->'
-// const prompts = template('I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is "<%= content %>"')
+// refs: https://github.com/f/awesome-chatgpt-prompts#act-as-an-english-translator-and-improver
+const prompts = template('I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is "<%= content %>"')
 
 export interface Comment {
   id: number
@@ -36,14 +37,21 @@ async function main() {
     const octokit = github.getOctokit(token)
     const { owner, repo } = github.context.repo
     const { number } = github.context.issue
-    debug('issue context %o', github.context.issue)
+    debug('issue context %O', github.context.issue)
+    const issue = await octokit.rest.issues.get({
+      issue_number: number,
+      owner,
+      repo,
+    })
+    const article = prompts({ content: issue.data.body })
+    debug('issue body with prompts %s', article)
     // list all <number> comments to find specific comments
     const comments = await octokit.rest.issues.listComments({
       owner,
       repo,
       issue_number: number,
     })
-    console.log(JSON.stringify(comments.data, null, 2))
+    debug('all comments %o from issue %s', comments.data, number)
     // find comments by bot<github_actioins> start with `<!--article-helper-->`
     const comment = findComment(comments.data)
     debug('find comment %o', comment)
