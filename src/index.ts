c32bc166ec66b442569ad515fe6848e4f8b09415
JiangWeixian <jiangweixian1994@gmail.com>
2023-02-12 09:39:21 +0800

    :construction:  list issue comments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import core from '@actions/core'
+import github from '@actions/github'
+import Debug from 'debug'
 import wait from './wait'
 
-// most @actions toolkit packages have async methods
-async function run() {
+const debug = Debug('chatgpt:article-review')
+
+async function main() {
   try {
     const ms = core.getInput('milliseconds')
     core.info(`Waiting ${ms} milliseconds ...`)
@@ -11,10 +14,32 @@ async function run() {
     await wait(parseInt(ms))
     core.info(new Date().toTimeString())
 
+    const token = process.env.GITHUB_TOKEN
+    if (!token) {
+      throw new Error('Github token is required.')
+    }
+    const octokit = github.getOctokit(token)
+    const { owner, repo } = github.context.repo
+    const { number } = github.context.issue
+    debug('issue context %o', github.context.issue)
+    octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: number,
+      body: 'hello world',
+    })
+    // list all <number> comments to find specific comments
+    const comments = await octokit.rest.issues.listComments({
+      owner,
+      repo,
+      issue_number: number,
+    })
+    console.log(comments)
+
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed((error as any).message)
   }
 }
 
-run()
+main()
